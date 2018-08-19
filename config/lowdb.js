bc5bb79f059304e493bc2df30d2f8d51b1ac9b91
zuperm4n <zahid@zmdev.com>
2018-08-19 13:17:44 +0100

    added users database to lowdb

--- config/lowdb.js
+++ config/lowdb.js
@@ -3,7 +3,9 @@ const FileSync = require('lowdb/adapters/FileSync');
 
 const baseDbPath = "./database";
 const apiKeys = low(new FileSync(`${baseDbPath}/api-keys.json`));
+const users = low(new FileSync(`${baseDbPath}/users.json`));
 
 module.exports = {
-    apiKeys: apiKeys
+    apiKeys: apiKeys,
+    users: users
 }
