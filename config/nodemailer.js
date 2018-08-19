53d341f8c4dd02020ebb6e85e641c4017b06cdd6
zuperm4n <zahid@zmdev.com>
2018-08-19 17:05:39 +0100

    cleaned up nodemailer config

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,7 +1,3 @@
-const db = {
-    config: require('../config/lowdb').config,
-}
-
 const nodemailer = require('nodemailer');
 
 module.exports = {
@@ -24,26 +20,18 @@ function sendMail(message) {
             pass: "Password!"
         }
     });
-
-    // setup email data with unicode symbols
+    
     let mailOptions = {
-        from: 'my@hostname.com', // sender address
-        to: 'to@domain.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
+        from: 'my@hostname.com',
+        to: message.to,
+        subject: message.subject,
+        text: message.text,
+        html: message.html
     };
-
-    // send mail with defined transport object
+    
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
         }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
 }
